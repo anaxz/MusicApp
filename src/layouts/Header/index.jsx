a86a6bc8e0ba2283ecdacb26ba4ba7a2576bf059
Anna <anaxz@users.noreply.github.com>
2022-11-28 15:46:31 +0000

    save current files

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -9,9 +9,9 @@ function Header() {
     return <>
         <h1>Music App</h1>
         <Navbar id='navbar'>
-            <NavLink activeClassName="active" className='nav-link' to='/'>Home</NavLink> 
-            <NavLink activeClassName="active" className='nav-link' to='artists'>Artists</NavLink>
-            <NavLink activeClassName="active" className='nav-link' to='song-list'>Songs List</NavLink>
+            <NavLink activateClassName="active" className='nav-link' to='/'>Home</NavLink> 
+            <NavLink activateClassName="active" className='nav-link' to='artists'>Artists</NavLink>
+            <NavLink activateClassName="active" className='nav-link' to='song-list'>Songs List</NavLink>
         </Navbar>
         <Outlet />
     </>
